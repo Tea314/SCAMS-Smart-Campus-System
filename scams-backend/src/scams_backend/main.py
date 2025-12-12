c6f1cec16f9130b7c96aa629ad723007f4bd6cd6
Tea314 <huynhkhoa03012004@gmail.com>
2025-12-12 07:04:34 +0000

    feat: add image_url to seed data - fix bug when retrieving data schedule - config cross site url in main.py

--- scams-backend/src/scams_backend/main.py
+++ scams-backend/src/scams_backend/main.py
@@ -1,8 +1,22 @@
 from scams_backend.core import config
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scams_backend.web_app import initialize_app
 
 app = FastAPI(title="Smart Campus System API", root_path="/api/v1")
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5173/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = initialize_app(app)
